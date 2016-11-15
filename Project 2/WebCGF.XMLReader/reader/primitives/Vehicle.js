2fa67e247cdf908581c8fde467a436225f559abd
Diogo Duque <diogoduque13@hotmail.com>
2016-11-15 11:04:32 +0000

    spaceship started!

--- Project 2/WebCGF.XMLReader/reader/primitives/Vehicle.js
+++ Project 2/WebCGF.XMLReader/reader/primitives/Vehicle.js
@@ -1,12 +1,12 @@
 /**
- * Vehicle
+ * Vehicle -> Spaceship
  * @param gl {WebGLRenderingContext}
  * @constructor
  */
 function Vehicle(scene) {
 	CGFobject.call(this,scene);
 
-    this.rectangle = new Rectangle(scene,0,0,10,10);
+    this.createLowerSpaceShip();
 
 }
 
@@ -14,7 +14,31 @@ Vehicle.prototype = Object.create(CGFobject.prototype);
 Vehicle.prototype.constructor=Vehicle;
 
 Vehicle.prototype.display = function () {
-    this.rectangle.display(); /*TO DO: draw a real vehicle!*/
+    this.loweSpaceShip.display(); /*TO DO: draw a real vehicle!*/
+}
+
+Vehicle.prototype.createLowerSpaceShip = function(){
+
+	var orderU = 2;
+	var orderV= 9;
+
+	var innerRadius = 5;
+	var outerRadius = 10;
+	var radiusDiff=outerRadius-innerRadius;
+	var height =3;
+
+	var controlPoints=[];
+	for(u=0; u<=orderU; u++){
+		var h=height*(u/orderU);
+		var radius = outerRadius - (u/orderU)*radiusDiff;
+		for(v=0; v<=orderV; v++){
+			var ang=(2*Math.PI)*(v/orderV);
+			controlPoints.push([radius*Math.cos(ang),h,radius*Math.sin(ang),1]);
+		}
+
+	}
+
+	this.loweSpaceShip = new Patch(this.scene,orderU,orderV,1,9,controlPoints);
 }
 
 
