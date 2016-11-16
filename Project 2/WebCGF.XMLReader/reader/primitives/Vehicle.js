bb4f46311b624f7d88ba7ee68f7d62bf621d8294
Diogo Duque <diogoduque13@hotmail.com>
2016-11-16 18:08:27 +0000

    more colors and FIRE :fire: :DDD

--- Project 2/WebCGF.XMLReader/reader/primitives/Vehicle.js
+++ Project 2/WebCGF.XMLReader/reader/primitives/Vehicle.js
@@ -9,9 +9,11 @@ function Vehicle(scene) {
 	this.domeAppearance = new CGFappearance(this.scene);
     this.domeAppearance.loadTexture("../res/white.jpg");
     this.legAppearance = new CGFappearance(this.scene);
-    this.legAppearance.loadTexture("../res/brick.jpg");
+    this.legAppearance.loadTexture("../res/black.jpg");
     this.hullAppearance = new CGFappearance(this.scene);
-    this.hullAppearance.loadTexture("../res/onix.jpg");
+    this.hullAppearance.loadTexture("../res/grey.jpg");
+    this.fireAppearance = new CGFappearance(this.scene);
+    this.fireAppearance.loadTexture("../res/bluefire.jpg");
 
 	this.innerRadius = 5; //not a real radius
 	this.outerRadius = 23;//not a real radius
@@ -20,8 +22,9 @@ function Vehicle(scene) {
     this.createHull(); //casco
     this.dome = new HalfSphere(scene,5,8); //cupula
     this.leg = new Cylinder(scene,.25,.25,6,5,5);//perna(s)
-    this.motor = new Cylinder(scene,1,2,4,8,5); //motor(es)
-    this.motorAux = new Cylinder(scene,.3,.3,3,5,3);
+    this.motor = new Cylinder(scene,1,2,4,7,5); //motor(es)
+    this.motorAux = new Cylinder(scene,.3,.3,3,5,3); //motor holder
+    this.fire = new Cylinder(scene,1.8,.1,5,7,5); //FOGO :DD
 
 }
 
@@ -35,35 +38,7 @@ Vehicle.prototype.display = function () {
     this.hullAppearance.apply();
     this.hull.display();
 
-    //dome
-    //this.scene.popMatrix();
-    //this.scene.pushMatrix();
-    this.domeAppearance.apply();
-    this.scene.translate(1,3.7,0);
-    this.scene.scale(4.1,4.6,3.8);
-    this.dome.display();
-
-    //motor holder
-    for(i=-1; i<=1; i+=2){
-    	this.scene.popMatrix();
-    	this.scene.pushMatrix();
-    	this.scene.translate(-1,3.3,i*4.5);
-    	if(i==-1)
-    		this.scene.rotate(-Math.PI*2/3,1,0,0);
-    	else this.scene.rotate(-Math.PI/3,1,0,0);
-    	this.motorAux.display();
-    }
-
-    //motor
-    for(i=-1; i<=1; i+=2){
-    	this.scene.popMatrix();
-    	this.scene.pushMatrix();
-    	this.scene.translate(0,6,i*6);
-    	this.scene.rotate(-Math.PI/2,0,1,0);
-    	this.motor.display();
-    }
-
-	//leg
+    //leg
 	this.legAppearance.apply();
     for(i=0;i<3; i++){
     	for(j=-1; j<=1; j+=2){
@@ -79,6 +54,46 @@ Vehicle.prototype.display = function () {
 			this.leg.display();
 		}
 	}
+
+    //dome
+    this.scene.popMatrix();
+    this.scene.pushMatrix();
+    this.domeAppearance.apply();
+    this.scene.translate(1,3.7,0);
+    this.scene.scale(4.1,4.6,3.8);
+    this.dome.display();
+
+    //motor holder
+    this.hullAppearance.apply();
+    for(i=-1; i<=1; i+=2){
+    	this.scene.popMatrix();
+    	this.scene.pushMatrix();
+    	this.scene.translate(-1,3.3,i*4.5);
+    	if(i==-1)
+    		this.scene.rotate(-Math.PI*2/3,1,0,0);
+    	else this.scene.rotate(-Math.PI/3,1,0,0);
+    	this.motorAux.display();
+    }
+
+    //motor
+    this.hullAppearance.apply();
+    for(i=-1; i<=1; i+=2){
+    	this.scene.popMatrix();
+    	this.scene.pushMatrix();
+    	this.scene.translate(0,6,i*6);
+    	this.scene.rotate(-Math.PI/2,0,1,0);
+    	this.motor.display();
+    }
+
+    //fire
+    this.fireAppearance.apply();
+    for(i=-1; i<=1; i+=2){
+    	this.scene.popMatrix();
+    	this.scene.pushMatrix();
+    	this.scene.translate(-4,6,i*6);
+    	this.scene.rotate(-Math.PI/2,0,1,0);
+    	this.fire.display();
+    }
 
 	this.scene.popMatrix();
 }
