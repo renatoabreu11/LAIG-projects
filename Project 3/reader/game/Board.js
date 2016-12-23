eed4fd2c2668c840c369f67c72a793b035cdc097
renatoabreu11 <renatoabreu__@hotmail.com>
2016-12-23 17:05:17 +0000

    comments

--- Project 3/reader/game/Board.js
+++ Project 3/reader/game/Board.js
@@ -10,8 +10,16 @@ function Board(board_encoded) {
     this.length = this.board.length;
 }
 
+/**
+ *
+ * @type {Board}
+ */
 Board.prototype.constructor = Board;
 
+/**
+ *
+ * @param board_encoded
+ */
 Board.prototype.plBoardParser = function (board_encoded) {
     var startIndex=2;
     while(board_encoded.length>1) {
@@ -23,22 +31,42 @@ Board.prototype.plBoardParser = function (board_encoded) {
     }
 };
 
+/**
+ *
+ * @returns {Number|*}
+ */
 Board.prototype.getBoardLength = function () {
     return this.length;
 }
 
+/**
+ *
+ * @param Row
+ * @param Col
+ * @returns {*}
+ */
 Board.prototype.getElementAt = function (Row, Col) {
     if(!this.validPosition(Row, Col))
         return "Invalid Position";
     return this.board[Row][Col];
 }
 
+/**
+ *
+ * @param Row
+ * @param Col
+ * @returns {boolean}
+ */
 Board.prototype.validPosition = function (Row, Col) {
     if(Row < 0 || Col <  0 || Col >= this.length || Row >= this.length)
         return false;
     return true;
 }
 
+/**
+ * 
+ * @returns {void|XML}
+ */
 Board.prototype.toPrologStruct = function () {
     var string = JSON.stringify(this.board);
     var boardPL = string.replace(/['"]+/g, '');
